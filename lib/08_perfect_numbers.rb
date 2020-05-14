380ff11fece35dccd6dfbda91e816758062ea4e3
Mark Henninger <markhenningerwork@gmail.com>
2020-05-14 15:18:42 +0200

    first draft challenge 9

--- lib/08_perfect_numbers.rb
+++ lib/08_perfect_numbers.rb
@@ -9,3 +9,50 @@
 # 28 is a perfect number because 28 = 1 + 2 + 4 + 7 + 14.
 # Prime numbers 7, 13, etc are deficient by the Nicomachus classification.
 # Write a program that can tell if a number is perfect, abundant or deficient.
+
+# Problem
+# write a class with on method
+# class has no initialize method
+#
+# class method called classify
+#   input: int
+#   output: string
+#
+#   The method checks if the input is a perfect number
+#   The output is deficient, perfect or abundat
+#
+#   What is a perfect number?
+#     a number that is the sum of its factors
+
+#   How to do that?
+#   Find the divisors
+#   add the divisors together and check if its t he number
+#   return string
+#
+#   How to find divisors?
+#   start at two and go up to number -1 to find all divisors
+#     check each number if mod is 0
+#     add that number to an array
+#
+#   sum up the array and compare with number (case)
+
+# if input is not a positive int raise RuntimeError
+
+class PerfectNumber
+  def self.classify(number)
+    raise RuntimeError if number <= 0
+
+    divisors = [1]
+    2.upto(number - 1) do |num|
+      divisors << num if number.modulo(num).zero?
+    end
+
+    if divisors.sum < number
+      'deficient'
+    elsif divisors.sum == number
+      'perfect'
+    else
+      'abundant'
+    end
+  end
+end
---
+++ ruby_challenges.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name        = 'ruby-challenges-Mark-Henninger'
+  s.version     = '1.0.0'
+  s.summary     = 'Solutions to the Ruby challenges of LS'
+  s.description = 'Solutions to the Ruby challenges of LS'
+  s.authors     = ['Mark Henninger']
+  s.email       = 'markhenningerwork@gmail.com'
+  s.homepage    = 'http://launchschool.com'
+  s.files       = ['lib/*', 'test/*']
+end
